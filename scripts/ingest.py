f4d1471b8be2af24bc7a598be64fabb858ea1df9
bipul <dahalbipul999@gmail.com>
2026-05-27 13:44:26 +0545

    Overlap added to ingestion pipeline

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -15,10 +15,13 @@ with open("Section_B_SDN.md","r", encoding="utf-8") as f:
     text = f.read()
 
 chunk_size = 200
+overlap = 50
 
-chunks =[
-    text[i:i+chunk_size] for i in range(0, len(text), chunk_size)
-]
+chunks = []
+
+for i in range(0, len(text), chunk_size - overlap):
+    chunk = text[i:i + chunk_size]
+    chunks.append(chunk)
 
 vectors = model.encode(chunks)
 
